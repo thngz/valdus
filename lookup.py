de277746bb2f9dba0027f5760c84143d232cfac3
lollipas <draakonlohe123@gmail.com>
2023-06-18 16:41:48 +0300

    cleaned code

--- lookup.py
+++ lookup.py
@@ -1,16 +1,15 @@
-import time
 import whois
 import concurrent.futures
 
 domains_path = "./domains.txt"
 available_domains = "./available_domains.txt"
+threads = 10
 
-with open(domains_path) as f:
-    domains = [line.strip() for line in f]
 
 def write_to_available(url: str):
     with open(available_domains, "a") as df:
         df.write(url + "\n")
+
 
 def perform_whois_lookup(domain):
     try:
@@ -18,7 +17,10 @@ def perform_whois_lookup(domain):
     except:
         write_to_available(domain)
 
-threads = 10
+
+with open(domains_path) as f:
+    domains = [line.strip() for line in f]
+
 with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
     futures = [executor.submit(perform_whois_lookup, domain) for domain in domains]
     concurrent.futures.wait(futures)
